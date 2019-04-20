3d0fc00a175d36596a33af0f89c6cf22be88f096
Andreas Schmidt <asc@e.mail.de>
2019-04-20 20:12:28 +0200

    dev, build and deploy

---
+++ thoschmiphotos-wallpaper.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "strings"
+import "github.com/mmcdole/gofeed"
+import "github.com/PuerkitoBio/goquery"
+import "github.com/reujab/wallpaper"
+
+func main() {
+	feed, _ := gofeed.NewParser().ParseURL("http://thoschmiphotos.blogspot.com/feeds/posts/default?alt=rss")
+	content := feed.Items[0].Description
+	fmt.Println(content)
+
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(content))
+	url, _ := doc.Find("a:has(img)").Attr("href")
+	fmt.Println(url)
+
+	wallpaper.SetFromURL(url)
+}
