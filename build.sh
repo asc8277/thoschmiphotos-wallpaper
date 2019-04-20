60febc23c1095baeaa92e486dad392c2fa42f27a
Andreas Schmidt <asc@e.mail.de>
2019-04-20 21:45:45 +0200

    vendor support

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=386 go build -o dist/thoschmiphotos-wallpaper-linux-x86 thoschmiphotos-wallpaper.go
-GOOS=linux GOARCH=amd64 go build -o dist/thoschmiphotos-wallpaper-linux-x64 thoschmiphotos-wallpaper.go
-GOOS=windows GOARCH=386 go build -o dist/thoschmiphotos-wallpaper-win-x86 thoschmiphotos-wallpaper.go
-GOOS=windows GOARCH=amd64 go build -o dist/thoschmiphotos-wallpaper-win-x64 thoschmiphotos-wallpaper.go
+GO111MODULE=on GOOS=linux GOARCH=386 go build -o dist/thoschmiphotos-wallpaper-linux-x86 thoschmiphotos-wallpaper.go
+GO111MODULE=on GOOS=linux GOARCH=amd64 go build -o dist/thoschmiphotos-wallpaper-linux-x64 thoschmiphotos-wallpaper.go
+GO111MODULE=on GOOS=windows GOARCH=386 go build -o dist/thoschmiphotos-wallpaper-win-x86 thoschmiphotos-wallpaper.go
+GO111MODULE=on GOOS=windows GOARCH=amd64 go build -o dist/thoschmiphotos-wallpaper-win-x64 thoschmiphotos-wallpaper.go
