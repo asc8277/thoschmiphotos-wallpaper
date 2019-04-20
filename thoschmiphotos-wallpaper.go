4fbaf18933abf8be787c12fb41a350ea7f52efe1
Andreas Schmidt <asc@e.mail.de>
2019-04-20 23:42:13 +0200

    tab 2 spaces

--- thoschmiphotos-wallpaper.go
+++ thoschmiphotos-wallpaper.go
@@ -7,13 +7,13 @@ import "github.com/PuerkitoBio/goquery"
 import "github.com/reujab/wallpaper"
 
 func main() {
-	feed, _ := gofeed.NewParser().ParseURL("http://thoschmiphotos.blogspot.com/feeds/posts/default?alt=rss")
-	content := feed.Items[0].Description
-	fmt.Println(content)
+  feed, _ := gofeed.NewParser().ParseURL("http://thoschmiphotos.blogspot.com/feeds/posts/default?alt=rss")
+  content := feed.Items[0].Description
+  fmt.Println(content)
 
-	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(content))
-	url, _ := doc.Find("a:has(img)").Attr("href")
-	fmt.Println(url)
+  doc, _ := goquery.NewDocumentFromReader(strings.NewReader(content))
+  url, _ := doc.Find("a:has(img)").Attr("href")
+  fmt.Println(url)
 
-	wallpaper.SetFromURL(url)
+  wallpaper.SetFromURL(url)
 }
